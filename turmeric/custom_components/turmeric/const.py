d67cac253e23cc78a7b2db0b00fcd63005ecd2a5
Kitra Drago <34182255+kitradrago@users.noreply.github.com>
2024-12-29 02:34:15 -0500

    Delete turmeric directory

--- turmeric/custom_components/turmeric/const.py
+++
@@ -1,6 +0,0 @@
-# const.py
-DOMAIN = "turmeric"
-CONF_API_TOKEN = "api_token"
-BASE_URL = "https://www.paprikaapp.com/api/v2/sync"
-UPDATE_INTERVAL_GROCERIES = 360  # Default: 6 hours
-UPDATE_INTERVAL_MEALS = 720  # Default: 12 hours
